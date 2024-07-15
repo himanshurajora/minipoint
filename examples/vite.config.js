be2c309a9c7aaa853a657eb88907c720a818b207
Himanshu Jangid <himanshujhar@gmail.com>
2024-07-15 12:37:24 +0530

    chore: Add delta time example page

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         mouse_inputs: resolve(__dirname, 'mouse-inputs/index.html'),
         update: resolve(__dirname, 'update/index.html'),
         keyboard_inputs: resolve(__dirname, 'keyboard-inputs/index.html'),
+        delta_time: resolve(__dirname, 'delta-time/index.html'),
       },
     },
   },
